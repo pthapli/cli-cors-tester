cd7703512900b12e4b104c7f89ceee05f1be9ecf
pthapli <pratyush@shapeman.co>
2022-06-24 23:08:11 +0530

    task-list image added to readme.md

---
+++ cors-tester/target/debug/deps/cors_tester-ba3154bb4ca51dde.d
@@ -0,0 +1,5 @@
+/Users/pthapli/Desktop/rust/cors-cli-tool/cli-cors-tester/cors-tester/target/debug/deps/cors_tester-ba3154bb4ca51dde.rmeta: src/main.rs
+
+/Users/pthapli/Desktop/rust/cors-cli-tool/cli-cors-tester/cors-tester/target/debug/deps/cors_tester-ba3154bb4ca51dde.d: src/main.rs
+
+src/main.rs:
