cace97efb91c649bf287606c369dcebc4466a741
pthapli <pratyush@shapeman.co>
2022-06-26 01:26:06 +0530

    fetched access-control-allow-origin header value

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use std::collections::HashMap;
-
 #[tokio::main]
 async fn main() {
     make_request("https://httpbin.org/ip").await.unwrap();
 }
 
 async fn make_request(url: &str) -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}",&url);
-    let resp = reqwest::get(url)
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let response = reqwest::get(url).await?;
+
+    let acao_header = response
+        .headers()
+        .get("access-control-allow-origin")
+        .unwrap();
+
+    println!("-----------------=================------------------");
+    println!("{:#?}", acao_header);
+
     Ok(())
 }
