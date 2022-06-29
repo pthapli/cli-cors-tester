7ad68172dfdf9c54902db98584fceae62c38632d
pthapli <pratyush@shapeman.co>
2022-06-30 00:16:41 +0530

    matched * to access-control-allow-origin header values

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,27 @@ async fn make_request(url: &str) -> Result<(), Box<dyn std::error::Error>> {
         .get("access-control-allow-origin")
         .unwrap();
 
-    println!("-----------------=================------------------");
-    println!("{:#?}", acao_header);
+    // println!("access-control-allow-origin : {:#?}", acao_header);
+    let acao = acao_header.to_str().unwrap();
+    let _is_cors_allowed = match_headers(&acao);
 
     Ok(())
+}
+
+fn match_headers(acao: &str) -> bool {
+    println!("access-control-allow-origin : {:#?}", acao);
+
+    /**
+     * @TODO 
+     * Get the origin ip address from the request and check if we get 
+     * the same response in access-control-allow-origin header 
+     **/
+ 
+    match acao {
+        "*" => println!("No CORS error for you my friend"),
+        _ => println!("Possible CORS, wait for the full code to be implemented"),
+    }
+
+    //Temporary
+    return false;
 }
